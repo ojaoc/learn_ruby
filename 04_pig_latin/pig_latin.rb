080d47056aa25854d4fbc62d6e6161d90e8e073b
ojaoc <c0rreia@hotmail.com>
2019-10-09 13:44:15 +0100

    04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,23 @@
 #write your code here
+
+def translate string
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    split_string = string.split(' ')
+    translated_string = ''
+    split_string.each_with_index { |element,index|
+        if vowels.include? element[0]
+            translated_string += ' ' + element + 'ay'
+        else
+            i = 0
+            while not vowels.include? element[i]
+
+                i += 1
+            end
+            if element[i] == 'u' && element[i-1] == 'q'
+                i += 1
+            end
+            translated_string += ' ' + element[i..] + element[0..i-1] + 'ay'
+        end
+    }
+    translated_string[1..]
+end
