765b90e7f3fc32117b2ec10699377743a277866d
ojaoc <c0rreia@hotmail.com>
2019-10-18 00:34:04 +0100

    05_books_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+    attr_accessor :title
+    def title=(book_title)
+        word = book_title.split(" ")
+        @title = String.new
+        exceptions = ["the", "a", "an", "and", "in", "of", "at", "for", "by"]
+        word.each_with_index do |x, index|
+            unless index == 0
+                if exceptions.include?(x)
+                    @title << x + " "
+                else
+                    @title << x.capitalize + " "
+                end
+            else
+                @title << x.capitalize + " "
+            end
+        end
+        @title.slice!(@title.length - 1)
+    end 
 end
